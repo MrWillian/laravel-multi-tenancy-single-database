0be43ad134407341a9ed107a3fadae37e8a45013
MrWilllian <willian.dev10@gmail.com>
2020-04-28 14:55:14 -0300

    refactor: implement index, create and store methods

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::get();
+        return view('posts.index', compact('posts'));
     }
 
     /**
@@ -24,7 +25,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
@@ -35,7 +36,8 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        auth()->user()->posts()->create($request->all());
+        return redirect()->route('posts.index');
     }
 
     /**
