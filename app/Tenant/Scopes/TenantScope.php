a9eb30a4bb6494b5a5ec55db45c4a76c22e5ff48
MrWilllian <willian.dev10@gmail.com>
2020-04-28 14:52:43 -0300

    refactor: add TenantScope

---
+++ app/Tenant/Scopes/TenantScope.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tenant\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use App\Tenant\ManagerTenant;
+
+class TenantScope implements Scope {
+  public function apply(Builder $builder, Model $model) {
+    $tenant = app(ManagerTenant::class)->identify();
+    $builder->where('tenant_id', $tenant);
+  }
+}
