5e4cdfd1e6fb2010a080f38819e4f4e84227bf1d
MrWilllian <willian.dev10@gmail.com>
2020-04-28 16:42:03 -0300

    refactor: add Middleware to check subdomain main

---
+++ app/Http/Middleware/Tenant/CheckSubdomainMain.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware\Tenant;
+
+use Closure;
+use App\Tenant\ManagerTenant;
+
+class CheckSubdomainMain
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $managerTenant = app(ManagerTenant::class);
+
+        if(!$managerTenant->isSubdomainMain()) {
+            abort(401);
+            return;
+        }
+
+        return $next($request);
+    }
+}
