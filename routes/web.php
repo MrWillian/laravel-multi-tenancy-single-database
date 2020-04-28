c64dd7ca9a21c47cc48ca2414966334b05822712
MrWilllian <willian.dev10@gmail.com>
2020-04-28 14:44:07 -0300

    refactor: add routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Route::get('posts', 'PostController@index')->name('posts.index');
+Route::resource('posts', 'PostController');
+
+Route::view('tenant-404', 'errors.404-tenant')->name('tenant.404');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
