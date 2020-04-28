d1c31ad803bbcc60bea47839fb6acf15161dbcb2
MrWilllian <willian.dev10@gmail.com>
2020-04-28 14:54:21 -0300

    refactor: add Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Tenant\Traits\TenantTrait;
 
 class Post extends Model
 {
-    //
+    use TenantTrait;
+    
+    protected $fillable = ['tenant_id', 'user_id', 'title', 'body', 'image'];
+ 
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tenant() {
+        return $this->belongsTo(Tenant::class);
+    }
 }
