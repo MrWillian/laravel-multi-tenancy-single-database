315ed5ae05784ff80e459bd21f8c3e1302e8a069
MrWilllian <willian.dev10@gmail.com>
2020-04-28 14:53:59 -0300

    refactor: add Tenant Model

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Tenant extends Model
 {
-    //
+    protected $fillable = ['subdomain', 'name'];
+
+    public function users() {
+        return $this->hasMany(User::class);
+    }
+
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
 }
