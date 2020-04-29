7e231d0d914d7843baffb07498e62163e7725966
MrWilllian <willian.dev10@gmail.com>
2020-04-29 11:06:42 -0300

    refactor: change Auth Routes to customize

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+//Auth::routes();
+
+/**
+ * Auth Custom Routes
+ */
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+
+// Registration Routes
+Route::group(['middleware' => 'subdomain_not_main'], function() {
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register')->name('');    
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
