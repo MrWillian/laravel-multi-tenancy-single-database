639891bc3f815b82883b8f9a79099eb7a59f0486
MrWilllian <willian.dev10@gmail.com>
2020-04-29 11:05:15 -0300

    refactor: add new middlewares to lists

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\Tenant\TenantMiddleware::class,
+        \App\Http\Middleware\Tenant\TenantFileSystems::class,
     ];
 
     /**
@@ -63,6 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'subdomain_main' => \App\Http\Middleware\Tenant\CheckSubdomainMain::class,
+        'subdomain_not_main' => \App\Http\Middleware\Tenant\SubdomainNotMain::class,
     ];
 
     /**
