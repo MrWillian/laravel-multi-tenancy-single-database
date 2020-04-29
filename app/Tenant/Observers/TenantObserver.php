25c831d549ea943752082586e1b9d6ec11762a4c
MrWilllian <willian.dev10@gmail.com>
2020-04-29 10:31:40 -0300

    refactor: add running in console verification

--- app/Tenant/Observers/TenantObserver.php
+++ app/Tenant/Observers/TenantObserver.php
@@ -7,6 +7,10 @@ use App\Tenant\ManagerTenant;
 
 class TenantObserver {
   public function creating(Model $model) {
+    if (app()->runningInConsole()) {
+      return;
+    }
+
     $tenant = app(ManagerTenant::class)->identify();
 
     $model->setAttribute('tenant_id', $tenant);
