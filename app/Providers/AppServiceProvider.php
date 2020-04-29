d214477bd9abffa888e3bc1af7d98369eaca5b9f
MrWilllian <willian.dev10@gmail.com>
2020-04-29 11:10:45 -0300

    refactor: add tenant and tenantmain Directives

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Blade;
+use App\Tenant\ManagerTenant;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+
+        /**
+         * Directives
+         */
+        $managerTenant = app(ManagerTenant::class);
+        Blade::if('tenant', function() use ($managerTenant) {
+            return !$managerTenant->isSubdomainMain();
+        });
+
+        Blade::if('tenantmain', function() use ($managerTenant) {
+            return $managerTenant->isSubdomainMain();
+        });
     }
 }
