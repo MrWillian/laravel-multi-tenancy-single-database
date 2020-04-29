7ee0a335ee0bb5f467446554ebe4d4e258cb37d4
MrWilllian <willian.dev10@gmail.com>
2020-04-29 11:12:12 -0300

    refactor: add boot method with generate uuid

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -3,11 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Webpatser\Uuid\Uuid;
 use App\User;
 
 class Tenant extends Model
 {
     protected $fillable = ['subdomain', 'name'];
+
+    public static function boot() {
+        parent::boot();
+        self::creating(function($model) {
+            $model->uuid = Uuid::generate();
+        });
+    }
 
     public function users() {
         return $this->hasMany(User::class);
